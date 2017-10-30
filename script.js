8c5f71760f5523e27ecdd689922bda4db26f9236
William Copeland <willcopeland34@gmail.com>
2017-10-29 19:44:58 -0700

    Housekeeping

--- script.js
+++ script.js
@@ -1,71 +1,23 @@
-var arr = [];
-
-function fetch() {
-
-	$.getJSON('https://ipinfo.io', function(data){
-		var weatherInfo =  data.loc.split(",");
-		var lon = Math.round(weatherInfo[0]);
-		var lat = Math.round(weatherInfo[1]);
-		console.log(lon, lat);
-
-	//url = "https://fcc-weather-api.glitch.me/api/current?lon=:" + arr[0]
-	
-	console.log("https://fcc-weather-api.glitch.me/api/current?lon=" + lon + "&lat=" + lat);
-	
-		$.getJSON("https://fcc-weather-api.glitch.me/api/current?lon=" + lon +"&lat=" + lat, function(data) {
-			
-			var temp = data.main.temp;
-			arr.push(data.main.temp);
-			$('#temp').append(temp);
-			var humidity = data.main.humidity;
-			arr.push(data.main.humidity);
-			$('#humidity').append(humidity);
-			var windSpd = data.wind.speed;
-			arr.push(data.wind.speed);
-			$('#windSpd').prepend(windSpd);
-			var weather = data.weather[0].description;
-			arr.push(data.weather[0].description);
-			$('#weather').append(weather);
-			console.log(arr);
-		});
-		 
-	});
-
-};
-
-
-
 function fetchOWM() {
 
 	$.getJSON('https://ipinfo.io', function(data){
-	
-		var weatherInfo = data.loc.split(",");
-		
+		var weatherInfo = data.loc.split(",");		
 		var lat = Math.round(weatherInfo[0]);
-		var lon = Math.round(weatherInfo[1]);
-		
+		var lon = Math.round(weatherInfo[1]);	
 		console.log(lon, lat);
 		
 		$.get("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon="+ lon +"&APPID=ec76489de971d221ea7a5c4e29102371", function(g) {
-		
-		$('.view').css('background','url("https://source.unsplash.com/1920x1080/?' +	g.weather[0].main.toLowerCase() + '")');
-		
+			$('.view').css('background','url("https://source.unsplash.com/1920x1080/?' +	g.weather[0].main.toLowerCase() + '")');	
 			$('#tempK').append(g.main.temp);
 			tempCalc();
-
-			$('#weather').append(g.weather[0].main);
-			
+			$('#weather').append(g.weather[0].main);	
 			$('#humidity').append(g.main.humidity);
-			$('#windSpd').append(g.wind.speed);
-			
-			
+			$('#windSpd').append(g.wind.speed);			
 		}); 
-	
 	});
 };
 
 function tempCalc() {
-
 	var tempK = $('#tempK').html();
 	$('#tempC').html(Math.round((tempK - 273.15), -1));
 	var tempC = $('#tempC').html();
@@ -77,25 +29,17 @@ function tempCalc() {
 };
 
 
-
 $(document).ready(function() {
-	//alert("Hellow");
+	fetchOWM();
 	
-	//if ($('#temp-unit').
-fetchOWM();
-//tempCalc();
-
 	$('#temp-unit').on('click', function() {
 		if ($('#temp-unit').is(':checked')) {
-			$('#F').show();
-			$('#C').hide(); console.log('1');
-			
+			$('#C').show();
+			$('#F').hide(); 	
 		}
 		else {
-			$('#C').show(); 
-			$('#F').hide();
-			console.log('2');
-			}
-			
+			$('#F').show(); 
+			$('#C').hide();
+			}	
 	});
 });
